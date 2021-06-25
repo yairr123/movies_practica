8c8de34fd518f104f33d134ca6d2cb0a03aea8f4
Rosales Bryan <18030602@itcelaya.edu.mx>
2021-06-25 16:55:01 -0500

    El primer commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+
+	import MovieItem from './Movie/Item.svelte'
+	export let name;
+	let texto = "Soy la data reactiva"
+</script>
+<svelte:head>
+	<title>Mis peliculas</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</svelte:head>
+<main class="conatiner">
+	<div class="row">
+		<MovieItem/>	
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
