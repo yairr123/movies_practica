5d20d6a380bf9873168370f5a60079c5ebffe453
Rosales Bryan <18030602@itcelaya.edu.mx>
2021-06-29 21:43:27 -0500

    Proyecto con un video Lo nuevo de la semana

--- src/App.svelte
+++ src/App.svelte
@@ -1,140 +1,271 @@
 <script>
-	import MoviItem from "./Movie/Item.svelte";
-	import { fly } from 'svelte/transition';
+  import MoviItem from "./Movie/Item.svelte";
+  import { fly } from "svelte/transition";
+  import { dataset_dev } from "svelte/internal";
 
-import { dataset_dev } from "svelte/internal";
-  
-	const APIKEY = "680ee273b817fc5b1516448d8ad88a21";  
-	const BASEURL = `https://api.themoviedb.org/3`;
-	const APISETTINGS = `?api_key=${APIKEY}&language=es-MX`;
+  const APIKEY = "680ee273b817fc5b1516448d8ad88a21";
+  const BASEURL = `https://api.themoviedb.org/3`;
+  const APISETTINGS = `?api_key=${APIKEY}&language=en-US`;
 
-	
-	//function fetchMovies() {
-	  //const URL = `${BASEURL}/discover/movie/${APISETTINGS}&sort_by=popularity.desc`;
-	  //fetch(URL)
-		//.then((res) => res.json())
-		//.then(({ results }) => {
-		  //movies = results;
-		  //console.log(movies);
-		//});
-	//}
+  const movies = (async () => {
+    const URL = `${BASEURL}/discover/movie/${APISETTINGS}&sort_by=popularity.desc`;
+    const response = await fetch(URL);
+    return await response.json();
+  })();
 
-	const movies = (async () =>{
-		const URL = `${BASEURL}/discover/movie/${APISETTINGS}&sort_by=popularity.desc`;
-		const response = await fetch(URL)
-		return await response.json()
-	})()
-  
-	let likedMovies=[]
+  let likedMovies = [];
 
-	function togglelike(event){
-		const movie = event.detail;
+  function togglelike(event) {
+    const movie = event.detail;
 
-		let index = likedMovies.findIndex(m => m.id === movie.id);
-		if(index >= 0){
-			likedMovies.splice(index, 1);
-			console.log(likedMovies);
-		}else{
-			likedMovies.push(movie);
+    let index = likedMovies.findIndex((m) => m.id === movie.id);
+    if (index >= 0) {
+      likedMovies.splice(index, 1);
+      console.log(likedMovies);
+    } else {
+      likedMovies.push(movie);
+    }
+    likedMovies = likedMovies;
+  }
+
+  $: like = (id) => {
+    let index = likedMovies.findIndex((m) => m.id === id);
+    return index >= 0;
+  };
+
+  let time = 0;
+	let duration;
+	let paused = true;
+
+	let showControls = true;
+	let showControlsTimeout;
+  		//Ver los controles tiempo
+	function handleMousemove(e) {
+		// Make the controls visible, but fade out after
+		// 2.5 seconds of inactivity
+		clearTimeout(showControlsTimeout);
+		showControlsTimeout = setTimeout(() => showControls = false, 2500);
+		showControls = true;
+
+		if (!(e.buttons & 1)) return; // mouse not down
+		if (!duration) return; // video not loaded yet
+
+		const { left, right } = this.getBoundingClientRect();
+		time = duration * (e.clientX - left) / (right - left);
+	}
+		//escuchar click
+	function handleMousedown(e) {
+		// we can't rely on the built-in click event, because it fires
+		// after a drag — we have to listen for clicks ourselves
+
+		function handleMouseup() {
+			if (paused) e.target.play();
+			else e.target.pause();
+			cancel();
 		}
-			likedMovies=likedMovies
-	}
 
-	$: like = (id) =>{
-		let index = likedMovies.findIndex(m => m.id === id);
-		return index >= 0;
-	}
+		function cancel() {
+			e.target.removeEventListener('mouseup', handleMouseup);
+		}
 
-  </script>
-  
-  <svelte:head>
-	<title>Movies Svelte</title>
-	<link
-	  rel="stylesheet"
-	  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
-	  integrity="undefined"
-	  crossorigin="anonymous"
-	/>
-  </svelte:head>
-  <main class="container">
-		<div class="row">
-			<div class="col-12 col-md-6 col-lg-8 border panel">
-				<h2>Peliculas populasres</h2>	
-		  		<div class="row">
-					{#await movies}
-						<!-- promise is pending -->
-						<div class="col-12">
-							<p>Cargando Datos . . . .</p>
-						</div>
-						{:then data}
-							{@debug data}
-							<!-- promise was fulfilled -->
-						{#each data.results as movie}
-							<div class="col-12 col-md-6 col-lg-4 p-2">
-								<MoviItem
-								    like={like(movie.id)}
-									id={movie.id}
-									title={movie.title}
-									overview={movie.overview}
-									cover={movie.poster_path}
-									on:onToggleLike={togglelike}
-								/>
-							</div>
-						{/each}
-					{/await}
-				</div>
-			</div>
-			<div class="col-12 col-md-6 col-lg-4 border panel " >
-				<h2>Peliculas favoritas</h2>
-				<div class="row"> 
-					{#if likedMovies.length}
-					{#each likedMovies as movie, i (movie.id)}
-							<div in:fly="{{duration:2000, y: 20}}" out:fly="{{duration:1000, y: -20}}" class="col-12 col-md-6 col-lg-4 p-2">
-								<MoviItem
-									like={like(movie.id)}
-									id={movie.id}
-									title={movie.title}
-									overview=""
-									cover={movie.cover}
-									on:onToggleLike={togglelike}
-								/>
-							</div>
-						{/each}
-						{:else}
-							<div class="col-12">
-								<p>
-									No Tienes peliculas favoritas...
-								</p>
-							</div>
-						{/if}
-				</div>
+		e.target.addEventListener('mouseup', handleMouseup);
+
+		setTimeout(cancel, 200);
+	}
+		//
+	function format(seconds) {
+		if (isNaN(seconds)) return '...';
+
+		const minutes = Math.floor(seconds / 60);
+		seconds = Math.floor(seconds % 60);
+		if (seconds < 10) seconds = '0' + seconds;
+
+		return `${minutes}:${seconds}`;
+	}
+ 	
+</script>
+
+<svelte:head>
+  <title>Movies Svelte</title>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+    integrity="undefined"
+    crossorigin="anonymous"
+  />
+</svelte:head>
+
+
+<center >
+	<h1 > Lo nuevo de la semana</h1>
+	<a href="https://www.themoviedb.org/tv/84958-loki">Haz click para descubrir más</a>
+	<div>
+		<video
+			poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
+			src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4"
+			on:mousemove={handleMousemove}
+			on:mousedown={handleMousedown}
+			bind:currentTime={time}
+			bind:duration
+			bind:paused>
+				<track kind="captions"/>
+		</video>
+	
+		<div class="controls" style="opacity: {duration && showControls ? 1 : 0}">
+			<progress value="{(time / duration) || 0}"/>
+	
+			<div class="info">
+				<span class="time">{format(time)}</span>
+				<span>click anywhere to {paused ? 'play' : 'pause'} / drag to seek</span>
+				<span class="time">{format(duration)}</span>
 			</div>
 		</div>
-  </main>
-  
-  <style>
-	  .panel{
-		  height: 100vh;
-		  overflow: auto;
-	  }
+	</div>
+	
+</center>
 
-	main {
-	  text-align: center;
-	  padding: 0;
-	  max-width: 240px;
-	  margin: 0 auto;
+
+<main class="container">
+  <div class="row">
+    <div class="col-12 col-md-6 col-lg-8 border panel">
+      <h2>Peliculas populares</h2>
+      <div class="row">
+        {#await movies}
+          <!-- promise is pending -->
+          <div class="col-12">
+            <p>Cargando Datos . . . .</p>
+          </div>
+        {:then data}
+          {@debug data}
+          <!-- promise was fulfilled -->
+          {#each data.results as movie}
+            <div class="col-12 col-md-6 col-lg-4 p-2">
+              <MoviItem
+                like={like(movie.id)}
+                id={movie.id}
+                title={movie.title}
+                overview={movie.overview}
+                cover={movie.poster_path}
+                on:onToggleLike={togglelike}
+              />
+            </div>
+          {/each}
+        {/await}
+      </div>
+    </div>
+    <div class="col-12 col-md-6 col-lg-4 border panel ">
+      <h2>Peliculas favoritas</h2>
+      <div class="row">
+        {#if likedMovies.length}
+          {#each likedMovies as movie, i (movie.id)}
+            <div
+              in:fly={{ duration: 2000, y: 20 }}
+              out:fly={{ duration: 1000, y: -20 }}
+              class="col-12 col-md-6 col-lg-4 p-2"
+            >
+              <MoviItem
+                like={like(movie.id)}
+                id={movie.id}
+                title={movie.title}
+                overview=""
+                cover={movie.cover}
+                on:onToggleLike={togglelike}
+              />
+            </div>
+          {/each}
+        {:else}
+          <div class="col-12">
+            <p>No Tienes peliculas favoritas...</p>
+          </div>
+        {/if}
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+	center{
+		background-color: dimgray;
 	}
+  .panel {
+    height: 100vh;
+    overflow: auto;
+  }
+
+  main {
+    text-align: center;
+    padding: 0;
+    max-width: 240px;
+    margin: 0 auto;
+    background-color: dimgray;
+  }
   
-	h1 {
-	  color: #ff3e00;
-	  text-transform: uppercase;
-	  font-size: 4em;
-	  font-weight: 100;
-	}
+
+  top{
+		font-family: sans-serif;
+        text-align: center;		
+		
+  }
   
-	@media (min-width: 640px) {
-	  main {
-		max-width: none;
-	  }
+
+  .controls {
+		align-items: center;
+		top: 100%;
+		width: 80%;
+		transition: opacity 1s;
 	}
-  </style>
+
+	.info {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+	}
+
+	span {
+		padding: 0.2em 0.5em;
+		color: white;
+		text-shadow: 0 0 8px black;
+		font-size: 1.4em;
+		opacity: 0.7;
+	}
+
+	.time {
+		width: 3em;
+	}
+
+	.time:last-child { text-align: right }
+
+	progress {
+		display: block;
+		width: 100%;
+		height: 10px;
+		-webkit-appearance: none;
+		appearance: none;
+	}
+
+	progress::-webkit-progress-bar {
+		background-color: rgba(0,0,0,0.2);
+	}
+
+	progress::-webkit-progress-value {
+		background-color: rgba(255,255,255,0.6);
+	}
+
+	video {
+		width: 80%;
+	}
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+	
+  }
+</style>
--- src/Movie/Item.svelte
+++ src/Movie/Item.svelte
@@ -1,27 +1,35 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from "svelte";
   export let id;
   export let title;
   export let overview;
   export let cover;
   export let like;
-  $: coverURL =`https://image.tmdb.org/t/p/w185_and_h278_bestv2${cover}`;
-  
-  const dispatch = createEventDispatcher();
-  
-  function togglelike (){
-    dispatch('onToggleLike',{id, title, overview, cover})
 
+  $: coverURL = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${cover}`;
+
+  const dispatch = createEventDispatcher();
+
+  function togglelike() {
+    dispatch("onToggleLike", { id, title, overview, cover });
   }
 </script>
 
-<div class="card">
-  <img src= {coverURL} class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">{title}</h5>
-  <p class="card-text">{overview}</p>
-<div class="text-center"> 
-    <button class={like ? 'btn btn-secondary': 'btn btn-primary'} on:click={togglelike}>{like ? 'Ya no me gusta' : 'Me gusta'}</button>
+
+<div
+  class="card text-white bg-dark mb-3 card border-dark mb-3 "
+  style="max-width: 18rem;"
+>
+  <img src={coverURL} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">{title}</h5>
+    <p class="card-text">{overview}</p>
+    <div class="text-center">
+      <button
+        class={like ? "btn btn-secondary" : "btn btn-primary"}
+        on:click={togglelike}>{like ? "Ya no me gusta" : "Me gusta"}</button
+      >
+    </div>
+  </div>
 </div>
-</div>
-</div>
+
